de32813ce86876f59e08d051aaf18b646e864ec2
Shahzeb khan <shahzeb.khan@sap.com>
2019-09-10 14:39:56 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,13 +1,36 @@
 exports.config = {
-    profile: "integration",        
-    baseUrl: "https://caa380-uiveri5demo2.cfapps.eu10.hana.ondemand.com/index.html",
-    auth: {
-      "sapcloud-form": {
-        user: "${params.user}",
-        pass: "${params.pass}",
-        userFieldSelector: 'input[name="username"]',
-        passFieldSelector: 'input[name="password"]',
-        logonButtonSelector: 'input[type="submit"]',
+  profile: "integration",        
+  baseUrl: "https://caa380-uiveri5demo2.cfapps.eu10.hana.ondemand.com/index.html",
+  auth: {
+    "sapcloud-form": {
+      user: "${params.user}",
+      pass: "${params.pass}",
+      userFieldSelector: 'input[name="username"]',
+      passFieldSelector: 'input[name="password"]',
+      logonButtonSelector: 'input[type="submit"]',
+    }
+  },
+  browsers: [{
+    browserName: 'chrome',
+    capabilities: {
+      chromedriverOptions: {
+        loggingTo: ['chromedriver.log']
+      },
+      chromeOptions: {
+          args: ['--disable-web-security',
+                   '--disable-gpu',
+                  '--disable-infobars',
+                '--disable-dev-shm-usage']
       }
     }
+    }],
+      connectionConfigs: {
+          direct: {
+              binaries: {
+                  chromedriver: {
+                      version: "75.0.3770.140"
+                  }
+              }
+          }
+      }
 };
