365592040c9cff10b27d067f1647c1e4253a31dc
Shahzeb khan <shahzeb.khan@sap.com>
2019-09-10 13:40:29 +0200

    Update teched.spec.js

--- teched.spec.js
+++ teched.spec.js
@@ -2,7 +2,16 @@ require('./pages/home');
 require('./pages/product');
 
 describe("teched", function () {
+  var originalTimeout;
 
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
 	it("should validate the home screen", function () {
         // Assertions
 		Then.onTheHomePage.iShouldSeeAllCategories();
