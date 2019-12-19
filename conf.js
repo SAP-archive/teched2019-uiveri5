2b833a49ef8ec1189302089831506b65e0637eb2
Shahzeb khan <shahzeb.khan@sap.com>
2019-12-19 13:04:23 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     profile: "integration",        
-    baseUrl: "https://sapui5nightly.int.sap.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
+    baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
    /*
    auth: {
       "sapcloud-form": {
