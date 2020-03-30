bc416a877c360c2ab48262ea4a04902513f9cf9e
Shahzeb khan <shahzeb.khan@sap.com>
2020-03-30 17:35:45 +0200

    Update node.js

--- node.js
+++ node.js
@@ -2,10 +2,13 @@ const execFile = require('child_process').execFile;
 
 function launchHeadlessChrome(url, callback) {
   // Assuming MacOSx.
-  const CHROME = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome';
+  const CHROME = '/usr/bin/google-chrome';
   execFile(CHROME, ['--headless', '--disable-gpu', '--remote-debugging-port=9222', url], callback);
 }
 
 launchHeadlessChrome('https://www.chromestatus.com', (err, stdout, stderr) => {
   console.log(stdout)
+    console.log(err)
+  console.log(stderr)
+
 });
