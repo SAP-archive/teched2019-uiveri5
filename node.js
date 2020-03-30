24aae12fc01713c366f54db76cc07bbeaa1f816a
Shahzeb khan <shahzeb.khan@sap.com>
2020-03-30 18:18:12 +0200

    Update node.js

--- node.js
+++ node.js
@@ -7,7 +7,7 @@ const url = argv.url || 'https://www.google.com';
 const format = argv.format === 'jpeg' ? 'jpeg' : 'png';
 const viewportWidth = argv.viewportWidth || 1440;
 const viewportHeight = argv.viewportHeight || 900;
-const delay = argv.delay || 0;
+const delay = argv.delay || 100;
 const userAgent = argv.userAgent;
 const fullPage = argv.full;
 
